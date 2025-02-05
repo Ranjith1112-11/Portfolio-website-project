746e6bac26e840841e762e3a562cae24d9fc2839
Ranjith1112-11 <ranjithkumar.d2001@gmail.com>
2025-02-05 14:19:28 +0530

    Fixed ignored files issue

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,22 +6,23 @@ const About = () => {
   return (
     <div
       id="About"
-      className="text-white flex flex-col items-center bg-black shadow-xl mx-0 md:mx-20 bg-opacity-30 rounded-lg p-12"
+      className="text-white flex flex-col items-center bg-black shadow-xl mx-0 md:mx-20 bg-opacity-30 rounded-lg p-6 md:p-12"
     >
-      
       <h2 className="text-2xl md:text-4xl font-bold w-full text-center mb-8">About</h2>
 
-      
       <div className="flex flex-col md:flex-row items-center justify-center w-full">
-     
-        <div className="md:w-1/2 flex justify-center">
-          <img className="md:h-80" src={AboutImg} alt="About img" />
+        {/* Image Section */}
+        <div className="md:w-1/2 w-full flex justify-center">
+          <img
+            className="w-full max-w-xs md:h-80 object-contain"
+            src={AboutImg}
+            alt="About img"
+          />
         </div>
 
-        
-        <div className="md:w-1/2 text-center md:text-left mt-6 md:mt-0">
+        {/* Text Section */}
+        <div className="md:w-1/2 w-full text-center md:text-left mt-6 md:mt-0 px-4">
           <ul>
-          
             <div className="flex items-start gap-3 py-4">
               <IoArrowForward size={30} className="mt-1" />
               <span>
@@ -32,7 +33,6 @@ const About = () => {
               </span>
             </div>
 
-           
             <div className="flex items-start gap-3 py-4">
               <IoArrowForward size={30} className="mt-1" />
               <span>
